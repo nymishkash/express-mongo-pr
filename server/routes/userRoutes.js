f32d0faf9d544b94df25573a96ebed6bcba719f3
nymishkash <reachnymish@gmail.com>
2024-07-10 10:45:08 +0530

    initial commit

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const User = require("../models/userModel");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    
+    try {
+
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        res.status(201).json('User created successfully');
+
+    } catch (error) {
+
+    }
+});
+
+router.post("/login", async (req, res) => {
+  
+});
+
+
+module.exports = router;
